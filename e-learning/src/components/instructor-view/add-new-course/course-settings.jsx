c8af0fc5fb15339e52f4bf94482a5aec84dc4a59
Roland550 <133446553+Roland550@users.noreply.github.com>
2025-04-10 22:51:23 +0100

    Handling upload image

--- e-learning/src/components/instructor-view/add-new-course/course-settings.jsx
+++ e-learning/src/components/instructor-view/add-new-course/course-settings.jsx
@@ -1,24 +1,57 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import { InstructorContext } from '@/context/instructor-context'
+import { mediaUploadService } from '@/service'
+import React, { useContext } from 'react'
 
 function CourseSettings() {
+  const{ coursLandingFormData, setCoursLandingFormData} = useContext(InstructorContext)
+
+  const handleImageUploadChange = async(event) => {
+    const seleTedImage = event.target.files[0]
+
+    if (seleTedImage) {
+      const imageFormData = new FormData()
+      imageFormData.append('file', seleTedImage)
+      try {
+        const response = await mediaUploadService(imageFormData)
+        console.log('resonse', response);
+        if(response.success){
+          setCoursLandingFormData({
+            ...coursLandingFormData,
+            image: response.data.url,
+          })
+        }
+        
+      } catch (error) {
+        console.error('Error uploading image:', error)
+        
+      }
+    }
+
+  }
   return (
     <Card>
        <CardHeader>
         <CardTitle>Course Settings</CardTitle>
        </CardHeader>
        <CardContent>
-        <div className="flex flex-col gap-3">
+       {
+        coursLandingFormData.image ? 
+          <img src={coursLandingFormData.image} alt="Course Image" />:
+          <div className="flex flex-col gap-3">
           <Label>Upload Course Image</Label>
           <Input
             type='file'
             accept='image/*'
-            
+            onChange={handleImageUploadChange} 
             
            />
         </div>
+        
+       }
+        
        </CardContent>
     </Card>
   )
--- e-learning/src/context/instructor-context/index.jsx
+++ e-learning/src/context/instructor-context/index.jsx
@@ -7,9 +7,7 @@ import { createContext, useState } from "react";
 export const InstructorContext = createContext(null);
 
 export default function InstructorProvider({ children }) {
-  const [coursLandingFormData, setCoursLandingFormData] = useState({
-    courseLandingInitialFormData,
-  });
+  const [coursLandingFormData, setCoursLandingFormData] = useState(courseLandingInitialFormData);
 
   const [courseCurriculumFormData, setCourseCurriculumFormData] = useState(
     courseCurriculumInitialFormData);
